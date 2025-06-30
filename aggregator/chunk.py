4f4c4b361fdbd6badfe518a2d2e9d707875c9398
Anett Hübner <anett_huebner@gmx.de>
2025-06-30 17:03:44 +0200

    removed internal variables

--- aggregator/chunk.py
+++ aggregator/chunk.py
@@ -2,16 +2,11 @@ from visionapi.sae_pb2 import Detection
 from visionapi.common_pb2 import GeoCoordinate
 
 class Chunk:
-    geo_coordinate: GeoCoordinate
-    x: float = 0.0
-    y: float = 0.0
-    time_in_ms: int = 0
-    class_id: int = 0
     
     def __init__(self, time_in_ms, detection: Detection) -> None:
         self.time_in_ms = time_in_ms
         self.geo_coordinate = GeoCoordinate()
-            
+      
         if detection is not None:
             self.geo_coordinate.CopyFrom(detection.geo_coordinate)
             self.class_id = detection.class_id
