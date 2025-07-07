780c9d477ecb4f574e47ccb676818a896c6d26ec
ztarbug <markus.zarbock@starwit.de>
2025-07-07 18:01:11 +0200

    workflow

--- start_make.sh
+++ start_make.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 export PATH=/root/.local/bin/:$PATH
-cd /code
 
 # Purpose of this script is to enforce a bash environment when calling make
-export PACKAGE_NAME=objectdetector
 export PATH=/root/.local/bin/:$PATH
+
 make test
 make build-deb
 
