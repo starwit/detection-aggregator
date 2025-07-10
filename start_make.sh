601eca15c6cb4f96a6e2d919fc687b999be85831
Anett Hübner <anett_huebner@gmx.de>
2025-07-10 18:27:20 +0200

    removed export poetry path

--- start_make.sh
+++ start_make.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-export PATH=/root/.local/bin/:$PATH
-
-# Purpose of this script is to enforce a bash environment when calling make
-export PATH=/root/.local/bin/:$PATH
-
 make test
 make build-deb
 
